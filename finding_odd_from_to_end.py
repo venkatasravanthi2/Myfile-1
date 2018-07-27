291744c530e858dd4db1ab4d3a17f37905a71ac6
battulakarthik <41747943+battulakarthik@users.noreply.github.com>
2018-07-27 22:08:33 +0530

    Update finding_odd_from_to_end.py

--- finding_odd_from_to_end.py
+++ finding_odd_from_to_end.py
@@ -2,7 +2,12 @@ import math
 nm,m=input().split()
 nm=int(nm)
 m=int(m)
+k=''
 if m < 10000:
     for i in range(nm+1,m):
-        if i % 2 != 0 and i != 0:
-            print(i,end=' ')
+        if i % 2 == 0 and i != 0:
+            if(i < m-2):
+                k=' '
+            else:
+                k=''
+            print(i,end=k)
