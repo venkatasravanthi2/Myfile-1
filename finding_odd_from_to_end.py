cabf59759320879befd4111e6a2cce475884b030
battulakarthik <41747943+battulakarthik@users.noreply.github.com>
2018-07-27 22:12:05 +0530

    Update finding_odd_from_to_end.py

--- finding_odd_from_to_end.py
+++ finding_odd_from_to_end.py
@@ -5,7 +5,7 @@ m=int(m)
 k=''
 if m < 10000:
     for i in range(nm+1,m):
-        if i % 2 == 0 and i != 0:
+        if i % 2 != 0 and i != 0:
             if(i < m-2):
                 k=' '
             else:
