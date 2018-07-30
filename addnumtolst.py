76da7c040690e4fc09b93d060a689c2480b23840
battulakarthik <41747943+battulakarthik@users.noreply.github.com>
2018-07-30 12:29:43 +0530

    Update addnumtolst.py

--- addnumtolst.py
+++ addnumtolst.py
@@ -1,11 +1,8 @@
 nm,k=input().split()
 nm=int(nm)
 k=int(k)
-a=[]
-for i in range(0,nm):
-    a.append(i+1)
-    print(a[i],end=' ')
-g=0
-for j in range(0,k):
-    g+=a[j]
-print(g)
+a = [int(x) for x in input().split()]
+s=0
+for i in range(0,k):
+    s+=a[i]
+print(s)
