ade17ec5571006790ab04e04b28444350c155fbe
battulakarthik <41747943+battulakarthik@users.noreply.github.com>
2018-08-01 23:07:15 +0530

    Update print first k digits.py

--- print first k digits.py
+++ print first k digits.py
@@ -1,7 +1,7 @@
-nm,k=input().split()
-k=int(k)
+nm,b=input().split()
+b=int(k)
 c=[]
-for i in range(0,k):
+for i in range(0,b):
     c.append(nm[i])
 for i in range(0,len(c)):
     print(c[i],end='')
