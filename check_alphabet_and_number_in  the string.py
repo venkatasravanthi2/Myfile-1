92ac21b5af9458d8e7ab7821a0a7b0526022c90b
battulakarthik <41747943+battulakarthik@users.noreply.github.com>
2018-08-01 22:27:04 +0530

    Create check_alphabet_and_number_in  the string.py

---
+++ check_alphabet_and_number_in  the string.py
@@ -0,0 +1,11 @@
+nm=input()
+count=0
+count1=0
+for i in range(0,len(nm)):
+    if (nm[i]>='a' and nm[i]<='z') or (nm[i]>'A' and nm[i]<'Z'):
+        count=1
+    elif (nm[i]=='1') or (nm[i]=='2') or (nm[i]=='3') or (nm[i]=='4') or (nm[i]=='5') or (nm[i]=='6') or (nm[i]=='7') or (nm[i]=='8') or (nm[i]=='9') or (nm[i]=='0'):
+        count1=1
+if count+count1==2:
+    print("yes")
+else:print("no")
